0ce4cb75c541e8c8f4fb9eb74f258a318319ab1d
hamidh-2k8 <hamidh.2k8@gmail.com>
2023-07-09 23:18:31 -0400

    Added simplified accel code (probably not work because cant math)

--- Kart Code/KartDriver.java
+++ Kart Code/KartDriver.java
@@ -1,8 +1,24 @@
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-public class KartDriver{
+public class KartDriver {
+    double accelConst;
+    private boolean isAccelerating = false;
 
-    
+    public KartDriver(double accelerationConstant) {
+        accelConst = accelerationConstant/10000000;
+    }
 
+    public float output;
+
+    public void accel(float input, ElapsedTime runtime){
+
+    isAccelerating = (input == 0.0);
+
+    if (isAccelerating) {
+        output = (output + runtime.seconds() * accelConst * input);
+    } else {
+        runtime.reset();
+        }
+    }
 }
