58d6db20c3921cb8cf5493c10623a32a3a3acc60
Hamid Hussain <116019826+hamidh-2k8@users.noreply.github.com>
2023-07-09 23:31:44 -0400

    fix dumb errors (i am redoing all of this)

--- Kart Code/TankExample.java
+++ Kart Code/TankExample.java
@@ -10,11 +10,14 @@ public class TankExample{
         DcMotor leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
         DcMotor rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
 
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
         yInput = gamepad1.right_trigger - gamepad1.left_trigger;
         rotInput = gamepad1.right_stick_x;
 
-        leftMotor.setPower(Range.clip(accel(yInput, time) + rotInput), -1.0, 1.0);
-        rightMotor.setPower(Range.clip(accel(yInput, time) - rotInput), -1.0, 1.0);
+        leftMotor.setPower(Range.clip(driver.accel(yInput, time) + rotInput), -1.0, 1.0);
+        rightMotor.setPower(Range.clip(driver.accel(yInput, time) - rotInput), -1.0, 1.0);
 
     }
 }
